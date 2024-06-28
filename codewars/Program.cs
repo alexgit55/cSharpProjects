4ede61787c04a9174f422f2d6160e1e7b5c2249d
alexgit55 <121723219+alexgit55@users.noreply.github.com>
2024-06-28 14:30:11 -0400

    Created new project for Calculator and Codewars

---
+++ codewars/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+long n = 35231;
+
+string s = n.ToString();
+char[] c = s.ToCharArray();
+Array.Reverse(c);
+long[] result = new long[c.Length];
+for (int i = 0; i < c.Length; i++)
+{
+    result[i] = long.Parse(c[i].ToString());
+}
+
+
+Console.ReadKey();
